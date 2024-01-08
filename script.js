25e866e70c23635b30646b960f0e3ce51c22aca7
TheMatX7 <thematx7@gmail.com>
2024-01-08 19:46:22 +0100

    mobi

--- script.js
+++
@@ -1,4 +0,0 @@
-function toggleContent(section) {
-    var content = document.getElementById('content' + section);
-    content.style.display = (content.style.display === 'none' || content.style.display === '') ? 'block' : 'none';
-  }
